02df0f4d30fc17c21f04c3874afdc9aa160591f3
George Stylianopoulos <65662416+GeorgeStyl@users.noreply.github.com>
2024-05-06 20:02:19 +0300

    created dedicated folder & files for QGIS scripts

---
+++ Python_Scripts/QGIS3_Scripts/startup.py
@@ -0,0 +1,19 @@
+from qgis.core import *
+import qgis.utils
+
+
+
+
+class QGIS_Env_Vars():
+    """
+        "startup.py" is the file which QGIS3 runs on start
+    
+    This is a class dedicated to QGIS environment variables
+    
+    Parameters:
+    - None
+    """
+    def __init__(self):
+        self.PYQGIS_STARTUP
+        
+    
